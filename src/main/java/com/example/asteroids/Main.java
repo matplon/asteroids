e839bf083ea43378447dc63e742caa4d37ff7c32
matplon <plonczak.mateusz@gmail.com>
2023-11-24 08:31:16 +0100

    New asteroids list

--- src/main/java/com/example/asteroids/Main.java
+++ src/main/java/com/example/asteroids/Main.java
@@ -11,6 +11,10 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
 
 public class Main extends Application {
 
@@ -19,13 +23,14 @@ public class Main extends Application {
     final double FPS = 30;
     final double CENTERX = (double) WIDTH /2, CENTERY = (double) HEIGHT /2;
     final double RADIUS = 15;
+    final double ASTEROID_COUNT = 6;
 
 
     AnchorPane root;
     Scene scene;
 
     Particle player;
-    List
+    List<Particle> asteroids;
 
 
     public void init(){
@@ -49,6 +54,15 @@ public class Main extends Application {
             if(keyEvent.getCode() == KeyCode.RIGHT || keyEvent.getCode() == KeyCode.LEFT) player.stopRotation(); // Stop rotating
         });
 
+        asteroids = new ArrayList<>();
+
+        for (int i = 0; i < ASTEROID_COUNT; i++) {
+            asteroids.add(new Particle(Math.random() * WIDTH, Math.random() * HEIGHT, Math.random() * 15, Math.random() * 6, Math.random() * 15,
+                    Math.random() * 360 - 180, false));
+            asteroids.get(i).setStroke(Color.WHITE);
+            root.getChildren().add(asteroids.get(i));
+        }
+
     }
 
     public void gameLogic(){
@@ -59,6 +73,9 @@ public class Main extends Application {
     public void start(Stage stage){
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(1000.0/FPS), actionEvent -> {
             player.updatePosition(FPS, WIDTH, HEIGHT); // Update player's position
+            for (int i = 0; i < asteroids.size(); i++) {
+                asteroids.get(i).updatePosition(FPS, WIDTH, HEIGHT);
+            }
         }));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
