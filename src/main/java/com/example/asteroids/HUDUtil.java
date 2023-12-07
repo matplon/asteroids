5218f2e90a5b8225a5925780808c4e06fccd5370
matplon <plonczak.mateusz@gmail.com>
2023-12-07 08:55:44 +0100

    class hudutil

--- src/main/java/com/example/asteroids/HUDUtil.java
+++ src/main/java/com/example/asteroids/HUDUtil.java
@@ -1,7 +1,45 @@
 package com.example.asteroids;
 
+import javafx.geometry.Pos;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+
+import java.util.List;
+
 public class HUDUtil {
-    public void addPoints(){
+    private static int points;
+    private static Text pointsText;
+    private static Text highScore;
+    private static List<Polygon> hearts;
+    private static Polygon heart;
+
+    public static void init(int previousHighScore) {
+        points = 0;
+
+        pointsText = new Text(points + "");
+        pointsText.setFont(Font.font("Times New Roman", 15));
+        pointsText.setTextAlignment(TextAlignment.CENTER);
+
+        highScore = new Text()
 
     }
+
+
+    public static void addPoints(int pointsAdd) {
+        points += pointsAdd;
+        pointsText.setText(points + "");
+    }
+
+    public static void removeHeart() {
+        hearts.remove(hearts.size() - 1);
+    }
+
+    public static void addHeart() {
+        hearts.add(heart);
+    }
+
+
 }
