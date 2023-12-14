21d63b2bde5313696abb452231112c99df78d85c
Computer Science <jan.kondracki9@gmail.com>
2023-12-14 09:40:53 +0100

    menu window

--- src/main/java/com/example/asteroids/MainController.java
+++ src/main/java/com/example/asteroids/MainController.java
@@ -1,11 +1,37 @@
 package com.example.asteroids;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.Label;
+import javafx.stage.Screen;
 
 public class MainController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private ImageView tempest;
+
+    @FXML
+    private ImageView first;
+
+    @FXML
+    private ImageView battleZone;
+
+
+    @FXML
+    void initialize() {
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        anchorPane.setPrefHeight(screenBounds.getHeight());
+        anchorPane.setPrefWidth(screenBounds.getWidth());
+    }
+
+
+
 
     @FXML
     protected void onHelloButtonClick() {
